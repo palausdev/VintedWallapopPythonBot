36992b5428968704e81b9c389cd87196c9932bbd
Pau Palacios <paupalacios@MacBook-Pro-de-Pau.local>
2023-06-13 19:15:02 +0200

    added config py + added code to function

--- main.py
+++ main.py
@@ -17,75 +17,80 @@ def obtener_dato_csv(ruta_archivo, fila, columna):
         dato = datos[fila][columna]
     return dato
 
-email = obtener_dato_csv("Sites/Wallapop.csv", 1, 0)
-pwd = obtener_dato_csv("Sites/Wallapop.csv", 1, 1)
-titleProduct = obtener_dato_csv("Sites/Wallapop.csv", 1, 3)
-price = obtener_dato_csv("Sites/Wallapop.csv", 1, 5)
-description = obtener_dato_csv("Sites/Wallapop.csv", 1, 10)
-shippingWeight = obtener_dato_csv("Sites/Wallapop.csv", 1, 14)
-shippingWeight = int(shippingWeight)
-shipping = obtener_dato_csv("Sites/Wallapop.csv", 1, 1)
+def addProduct():
 
-chrome_options = Options()
-#chrome_options.add_argument("--headless")
-chrome_options.add_experimental_option("detach", True)
+    email = obtener_dato_csv("Sites/Wallapop.csv", 1, 0)
+    pwd = obtener_dato_csv("Sites/Wallapop.csv", 1, 1)
+    titleProduct = obtener_dato_csv("Sites/Wallapop.csv", 1, 3)
+    price = obtener_dato_csv("Sites/Wallapop.csv", 1, 5)
+    description = obtener_dato_csv("Sites/Wallapop.csv", 1, 10)
+    shippingWeight = obtener_dato_csv("Sites/Wallapop.csv", 1, 14)
+    shippingWeight = int(shippingWeight)
+    shipping = obtener_dato_csv("Sites/Wallapop.csv", 1, 1)
 
-driver = webdriver.Chrome(chrome_options)
+    chrome_options = Options()
+    #chrome_options.add_argument("--headless")
+    chrome_options.add_experimental_option("detach", True)
 
-driver.get("https://es.wallapop.com/login")
-time.sleep(1)
+    driver = webdriver.Chrome(chrome_options)
 
-driver.find_element(By.CLASS_NAME,'Welcome__btn-go-login-form').click()
+    driver.get("https://es.wallapop.com/login")
+    time.sleep(1)
 
-driver.find_element(By.ID, 'email').send_keys(email)
-time.sleep(6)
-driver.find_element(By.ID, 'password').send_keys(pwd)
+    driver.find_element(By.CLASS_NAME,'Welcome__btn-go-login-form').click()
 
-mainMenu = False
+    driver.find_element(By.ID, 'email').send_keys(email)
+    time.sleep(6)
+    driver.find_element(By.ID, 'password').send_keys(pwd)
 
-while mainMenu == False:
-    if driver.current_url == 'https://es.wallapop.com/wall': mainMenu = True
+    mainMenu = False
 
-driver.get('https://es.wallapop.com/app/catalog/upload')
-time.sleep(2)
-driver.find_element(By.ID, 'onetrust-accept-btn-handler').click()
-driver.find_element(By.XPATH, '/html/body/tsl-root/tsl-private/div/div/div/tsl-upload/div/div/tsl-category-selector/div/div[3]/div/a[1]').click()
+    while mainMenu == False:
+        if driver.current_url == 'https://es.wallapop.com/wall': mainMenu = True
 
-driver.find_element(By.ID, 'headline').send_keys(titleProduct)
-time.sleep(3)
-driver.find_element(By.XPATH, '//*[@id="objectType"]/div/div/div/div[1]').click()
-time.sleep(1)
-driver.find_element(By.XPATH, '//*[@id="objectType"]/div/tsl-dropdown-list/div/div[2]/ul/li[7]').click()
-time.sleep(1)
-driver.find_element(By.XPATH, '//*[@id="objectType2"]/div/div/div/div[1]').click()
-time.sleep(1)
-driver.find_element(By.XPATH, '//*[@id="objectType2"]/div/tsl-dropdown-list/div/div/ul/li[3]').click()
-time.sleep(1)
-driver.find_element(By.ID, 'price').send_keys(price)
-time.sleep(1)
-driver.find_element(By.XPATH, '//*[@id="conditions"]/div/div/div/div[1]').click()
-time.sleep(1)
-driver.find_element(By.XPATH, '//*[@id="conditions"]/div/tsl-dropdown-list/div/div[2]/ul/li[1]').click()
-time.sleep(2)
-driver.find_element(By.XPATH, '//*[@id="tellUs"]').send_keys(description)
+    driver.get('https://es.wallapop.com/app/catalog/upload')
+    time.sleep(2)
+    driver.find_element(By.ID, 'onetrust-accept-btn-handler').click()
+    driver.find_element(By.XPATH, '/html/body/tsl-root/tsl-private/div/div/div/tsl-upload/div/div/tsl-category-selector/div/div[3]/div/a[1]').click()
 
-driver.find_element(By.XPATH, '/html/body/tsl-root/tsl-private/div/div/div/tsl-upload/div/div/tsl-upload-product/form/div[2]/tsl-drop-area/div/div[2]/div/div[1]/label/input').send_keys('/Users/paupalacios/Downloads/Logo-TowerDefenseRuine.jpg')
+    driver.find_element(By.ID, 'headline').send_keys(titleProduct)
+    time.sleep(3)
+    driver.find_element(By.XPATH, '//*[@id="objectType"]/div/div/div/div[1]').click()
+    time.sleep(1)
+    driver.find_element(By.XPATH, '//*[@id="objectType"]/div/tsl-dropdown-list/div/div[2]/ul/li[7]').click()
+    time.sleep(1)
+    driver.find_element(By.XPATH, '//*[@id="objectType2"]/div/div/div/div[1]').click()
+    time.sleep(1)
+    driver.find_element(By.XPATH, '//*[@id="objectType2"]/div/tsl-dropdown-list/div/div/ul/li[3]').click()
+    time.sleep(1)
+    driver.find_element(By.ID, 'price').send_keys(price)
+    time.sleep(1)
+    driver.find_element(By.XPATH, '//*[@id="conditions"]/div/div/div/div[1]').click()
+    time.sleep(1)
+    driver.find_element(By.XPATH, '//*[@id="conditions"]/div/tsl-dropdown-list/div/div[2]/ul/li[1]').click()
+    time.sleep(2)
+    driver.find_element(By.XPATH, '//*[@id="tellUs"]').send_keys(description)
+
+    driver.find_element(By.XPATH, '/html/body/tsl-root/tsl-private/div/div/div/tsl-upload/div/div/tsl-upload-product/form/div[2]/tsl-drop-area/div/div[2]/div/div[1]/label/input').send_keys('/Users/paupalacios/Downloads/Logo-TowerDefenseRuine.jpg')
 
 
-if shipping == False: driver.find_element(By.XPATH, '//*[@id="b7f8ad8f-33a6-4d13-9c6b-ce70a26fd322"]').click()
+    if shipping == False: driver.find_element(By.XPATH, '//*[@id="b7f8ad8f-33a6-4d13-9c6b-ce70a26fd322"]').click()
 
-if shippingWeight > 20 and shippingWeight <= 30:
-    driver.find_element(By.XPATH, '//*[@id="4"]').click()
-elif shippingWeight > 10 and shippingWeight <= 20:
-    driver.find_element(By.XPATH, '//*[@id="3"]').click()
-elif shippingWeight > 5 and shippingWeight <= 10:
-    driver.find_element(By.XPATH, '//*[@id="2"]').click()
-elif shippingWeight > 2 and shippingWeight <= 5:
-    driver.find_element(By.XPATH, '//*[@id="1"]').click()
-elif shippingWeight > 0 and shippingWeight <= 2:
-    driver.find_element(By.XPATH, '//*[@id="0"]').click()
+    if shippingWeight > 20 and shippingWeight <= 30:
+        driver.find_element(By.XPATH, '//*[@id="4"]').click()
+    elif shippingWeight > 10 and shippingWeight <= 20:
+        driver.find_element(By.XPATH, '//*[@id="3"]').click()
+    elif shippingWeight > 5 and shippingWeight <= 10:
+        driver.find_element(By.XPATH, '//*[@id="2"]').click()
+    elif shippingWeight > 2 and shippingWeight <= 5:
+        driver.find_element(By.XPATH, '//*[@id="1"]').click()
+    elif shippingWeight > 0 and shippingWeight <= 2:
+        driver.find_element(By.XPATH, '//*[@id="0"]').click()
 
-driver.find_element(By.XPATH, '//*[@id="prueba"]/div[1]/walla-button').click()
+    driver.find_element(By.XPATH, '//*[@id="prueba"]/div[1]/walla-button').click()
+    return driver
+
+addProduct()
 
 
 
