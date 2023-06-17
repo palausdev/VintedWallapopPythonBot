4b71753d0ece7b3013c98eafa7cbbe258521152f
Pau Palacios <paupalacios@MBP-de-Pau.home>
2023-06-17 18:03:12 +0200

    tweaks + discord webhook

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # This is a sample Python script.
+import array
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
+from discordwebhook import Discord
 from pypresence import Presence
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -24,7 +26,7 @@ RPC.update(
 )
 print('\033[95m Starting...')
 def addProduct(email, password, title, price, currency, category, subcategory, specify, productState, description, hashtags, photoFolderPath, shipping, weight):
-    hora_inicio = datetime.datetime.now()
+    discord = Discord(url="https://discord.com/api/webhooks/1119628512597377024/KCYXUjQ3oR_vuxEt65qJd3_iMPA6_sNGTg-4mCbIvrktwEkUjSl8xGh3xCksOvllUGAs")
     shippingWeight = int(weight)
 
     chrome_options = Options()
@@ -71,12 +73,22 @@ def addProduct(email, password, title, price, currency, category, subcategory, s
 
     print('\033[96m Logged in!')
 
+    hora_inicio = datetime.datetime.now()
     driver.get('https://es.wallapop.com/app/catalog/upload')
     time.sleep(2)
     #Accept private settings button
     driver.find_element(By.ID, 'onetrust-accept-btn-handler').click()
-    #Click on option to sell product
-    driver.find_element(By.XPATH, '/html/body/tsl-root/tsl-private/div/div/div/tsl-upload/div/div/tsl-category-selector/div/div[3]/div/a[1]').click()
+
+    time.sleep(1)
+    productBool = False
+    while productBool == False:
+        try:
+            #Click on option to sell product
+            driver.find_element(By.XPATH, '/html/body/tsl-root/tsl-private/div/div/div/tsl-upload/div/div/tsl-category-selector/div/div[3]/div/a[1]').click()
+            print('\033[96m Inside list module!')
+            break
+        except:
+            print('\033[93m Trying to access the list...')
 
     print('\033[93m Inserting Title...')
     try:
@@ -86,43 +98,51 @@ def addProduct(email, password, title, price, currency, category, subcategory, s
     except:
         print('\033[91m Title Input Failed!')
 
-    time.sleep(3)
+    time.sleep(2)
     #Click Category
     driver.find_element(By.XPATH, '//*[@id="category"]/div').click()
-    time.sleep(1)
+    #time.sleep(1)
     #Select Category
     driver.find_element(By.XPATH, category).click()
-    time.sleep(1)
+    #time.sleep(1)
     #Click Subcategory
     driver.find_element(By.XPATH, '//*[@id="objectType"]/div').click()
-    time.sleep(1)
+    #time.sleep(1)
 
-    #Select Category
-    if subcategory is not None: driver.find_element(By.XPATH, subcategory).click()
+    subcategoryBool = False
+    while subcategoryBool == False:
+        print('\033[93m Selecting Subcategory...')
+        try:
+            # Select SubCategory
+            if subcategory is not None: driver.find_element(By.XPATH, subcategory).click()
+            print('\033[92m Subcategory Selected Correctly!')
+            break
+        except:
+            print('\033[91m Select Subcategory Failed!')
 
-    time.sleep(1)
+    #time.sleep(1)
 
     #Click Specify
     driver.find_element(By.XPATH, '//*[@id="objectType2"]/div/div/div/div[1]').click()
-    time.sleep(1)
+    #time.sleep(1)
 
     #Select Specify
     if specify is not None: driver.find_element(By.XPATH, specify).click()
 
 
-    time.sleep(1)
+    #time.sleep(1)
     #Price
     driver.find_element(By.ID, 'price').send_keys(price)
-    time.sleep(1)
+    #time.sleep(1)
     #Click Condition
     driver.find_element(By.XPATH, '//*[@id="conditions"]/div/div/div/div[1]').click()
-    time.sleep(1)
+    #time.sleep(1)
     #Select Condition
-    driver.find_element(By.XPATH, '//*[@id="conditions"]/div/tsl-dropdown-list/div/div[2]/ul/li[1]').click()
-    time.sleep(2)
+    driver.find_element(By.XPATH, productState).click()
+    #time.sleep(2)
     #Description
     driver.find_element(By.XPATH, '//*[@id="tellUs"]').send_keys(description)
-    time.sleep(2)
+    #time.sleep(2)
     photoFolder = photoFolderPath
     contenido = os.listdir(photoFolder)
     #images = []
@@ -137,6 +157,7 @@ def addProduct(email, password, title, price, currency, category, subcategory, s
     #Condition to click on shipping button
     if shipping == False: driver.find_element(By.XPATH, '//*[@id="b7f8ad8f-33a6-4d13-9c6b-ce70a26fd322"]').click()
 
+    time.sleep(1)
     #Condition to select the package weight
     if shippingWeight > 20 and shippingWeight <= 30:
         driver.find_element(By.XPATH, '//*[@id="4"]').click()
@@ -152,12 +173,34 @@ def addProduct(email, password, title, price, currency, category, subcategory, s
     #Publish product button
     print('\033[93m Uploading Product...')
     try:
+        segundos_transcurridos = (datetime.datetime.now() - hora_inicio).total_seconds()
+        segundos_transcurridos = str(round(segundos_transcurridos, 2))
         driver.find_element(By.XPATH, '//*[@id="prueba"]/div[1]/walla-button').click()
         print('\033[96m Product Uploaded Successfully!')
+        discord.post(
+            username="SnapSell",
+            avatar_url="https://cdn.discordapp.com/attachments/718875492744298569/1119595752738521259/LogoSnapSell.png",
+            embeds=[
+                {
+                    "title": f"Product: {title} Uploaded!",
+                    "fields": [
+                        {"name": "Module", "value": "Wallapop", "inline": True},
+                        {"name": "Mode", "value": "Fast Mode", "inline": True},
+                        {"name": "Upload Time", "value": f"{segundos_transcurridos}", "inline": True},
+                    ],
+                    "footer": {
+                        "text": f"SnapSell | Wallapop Module · {datetime.datetime.now()} ",
+                        "icon_url": "https://cdn.discordapp.com/attachments/718875492744298569/1119595752738521259/LogoSnapSell.png",
+                    },
+                }
+            ],
+            #content=f"Product {title} Uploaded Successfully",
+
+        )
     except:
         print('\033[91m Product Upload Failed!')
 
-    segundos_transcurridos = (datetime.datetime.now() - hora_inicio).total_seconds()
+    #segundos_transcurridos = (datetime.datetime.now() - hora_inicio).total_seconds()
 
     print(f'\033[95m Upload time: {segundos_transcurridos}')
 
