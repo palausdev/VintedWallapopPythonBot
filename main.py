50673212b0665ada87c60d81efca9155a639dfc8
ByPalacios <paupalacioseso@gmail.com>
2023-06-21 13:49:40 +0200

    Added Menu function inside key validation

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from pypresence import Presence
 from config import wallapopData, wallapopCategorySwitch, wallapopSubcategorySwitch, wallapopSpecifySwitch, wallapopConditionSwitch
-import requests,json,sys
+import requests,json,sys, os
 from wallapop import addProduct
 from pyfiglet import figlet_format
+
+print("Validating Key...")
 
 sys.stdout.write("\x1b]2;SnapSell - Menu\x07")
 
 result = figlet_format("SnapSell", font = "big")
-print(result)
+
+
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+# now, to clear the screen
+cls()
 
 client_id = "1119565049208508456"
 RPC = Presence(client_id)
@@ -32,14 +40,15 @@ with open(json_file, 'r') as file:
 webhook_value = data["webhook"]
 key_value = data["key"]
 
-print("---------------------------")
-print("Select an option:")
-print("[1] Wallapop")
-print("[2] Vinted")
-print("[3] Config")
-print("[0] Exit")
-print("---------------------------")
-option = input(">> ")
+def menu():
+    print(result)
+    print("---------------------------")
+    print("Select an option:")
+    print("[1] Wallapop")
+    print("[2] Vinted")
+    print("[3] Config")
+    print("[0] Exit")
+    print("---------------------------")
 
 def authKey(key):
     url = f"https://api.whop.com/api/v2/memberships/{key}/validate_license"
@@ -56,18 +65,26 @@ def authKey(key):
     return response
 
 if authKey(key_value).status_code == 201:
-    print('\033[95m Starting...')
-    rowcount = 0
-    for row in open("Sites/wallapop.csv"):
-        rowcount += 1
-        addProduct(wallapopData(rowcount)[0], wallapopData(rowcount)[1], wallapopData(rowcount)[2],
-                   wallapopData(rowcount)[3], wallapopData(rowcount)[4],
-                   wallapopCategorySwitch(wallapopData(rowcount)[5]),
-                   wallapopSubcategorySwitch(wallapopData(rowcount)[5], wallapopData(rowcount)[6]),
-                   wallapopSpecifySwitch(wallapopData(rowcount)[5], wallapopData(rowcount)[6],
-                                         wallapopData(rowcount)[7]), wallapopConditionSwitch(wallapopData(rowcount)[8]),
-                   wallapopData(rowcount)[9], wallapopData(rowcount)[10], wallapopData(rowcount)[11],
-                   wallapopData(rowcount)[12], wallapopData(rowcount)[13], webhook_value)
+    cls()
+    menu()
+    option = input(">> ")
+
+    while option != 0:
+        if option == 1:
+            print('\033[95m Starting...')
+            rowcount = 0
+            for row in open("Sites/wallapop.csv"):
+                rowcount += 1
+                addProduct(wallapopData(rowcount)[0], wallapopData(rowcount)[1], wallapopData(rowcount)[2],
+                           wallapopData(rowcount)[3], wallapopData(rowcount)[4],
+                           wallapopCategorySwitch(wallapopData(rowcount)[5]),
+                           wallapopSubcategorySwitch(wallapopData(rowcount)[5], wallapopData(rowcount)[6]),
+                           wallapopSpecifySwitch(wallapopData(rowcount)[5], wallapopData(rowcount)[6],
+                                                 wallapopData(rowcount)[7]), wallapopConditionSwitch(wallapopData(rowcount)[8]),
+                           wallapopData(rowcount)[9], wallapopData(rowcount)[10], wallapopData(rowcount)[11],
+                           wallapopData(rowcount)[12], wallapopData(rowcount)[13], webhook_value)
+
+    quit()
 elif authKey(key_value).status_code == 401:
     print('\033[91m Reset your key in Dashboard')
 elif authKey(key_value).status_code == 400:
