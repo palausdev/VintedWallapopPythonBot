ee2f48a3234bcd49da98e25d7608390a0490aa44
Pau Palacios <paupalacioseso@gmail.com>
2023-06-30 11:05:41 +0200

    Dark Window with button

--- gui.py
+++ gui.py
@@ -1,8 +1,18 @@
+import customtkinter
 import customtkinter as ctk
+
+customtkinter.set_appearance_mode("dark")
+
+
+def button_function():
+    print('Button pressed')
 
 def gui():
     root = ctk.CTk()
     root.geometry("750x450")
     root.title("SnapSell")
+    root.iconbitmap(r'LogoSnapSellExe.ico')
+    button = customtkinter.CTkButton(master=root, text="Button", command=button_function)
+    button.place(relx=0.5, rely=0.5, anchor=customtkinter.CENTER)
 
     return root.mainloop()
